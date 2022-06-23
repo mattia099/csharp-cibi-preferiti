b464be56b7ee6b95aaf5202bf34b1e6ad5c1e061
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-06-23 15:15:20 +0200

    esercizio finito + bonus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] cibi = { "Lasagna", "Tiramisù", "Arancino", "Pizza", "Gelato" };
+
+Console.WriteLine(cibi.Length);
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine($"{i + 1}. {cibi[i]}");
+}
+
+Console.WriteLine($"Primo cibo in calssifica: {cibi[0]}");
+
+Console.WriteLine($"Ultimo cibo in calssifica: {cibi[cibi.Length - 1]}");
+
+if (cibi.Length % 2 == 0)
+{
+    Console.WriteLine($"Cibo di metà classifica: {cibi[cibi.Length / 2]} {cibi[cibi.Length / 2 - 1]}");
+}
+else {
+    Console.WriteLine($"Cibo di metà classifica: {cibi[cibi.Length / 2]}");
+}
+
+
